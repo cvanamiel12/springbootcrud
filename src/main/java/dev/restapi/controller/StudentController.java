28148b81d54bc4673ebaab8410681801af711086
cvanamiel12 <cvanamiel@gmail.com>
2025-02-19 00:49:11 +0800

    Added fetching student using id

--- src/main/java/dev/restapi/controller/StudentController.java
+++ src/main/java/dev/restapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import dev.restapi.entity.Student;
 import dev.restapi.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +20,13 @@ public class StudentController {
     public List<Student> getAllStudents() {
         List<Student> students = repo.findAll();
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        Student student = repo.findById(id).get();
+
+        return student;
     }
 
 }
